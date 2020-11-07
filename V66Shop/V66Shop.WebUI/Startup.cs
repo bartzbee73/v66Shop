4a9fd3d689339b9ed979612ff4b511c4554fb2e8
bartzbee73 <57739349+bartzbee73@users.noreply.github.com>
2020-11-07 16:10:34 +0800

    Initial Solution Setup

---
+++ V66Shop/V66Shop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(V66Shop.WebUI.Startup))]
+namespace V66Shop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
